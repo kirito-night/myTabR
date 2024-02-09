76a2595a5dd821cd5ec600ce046c61a6d8130d7a
ZENG <28600693@ppti-14-302-13.ufr-info-p6.jussieu.fr>
2024-02-09 17:39:29 +0100

    result of exp2

--- result.py
+++ result.py
@@ -3,7 +3,7 @@ import numpy as np
 
 percs = [0.01, 0.02, 0.04, 0.08, 0.16, 0.24, 0.32, 0.42]
 
-for num in range(4):
+for num in range(5):
     S = [np.loadtxt(f'./log/{num}_{i}.log') for i in range(2)]
     S = np.array(S)
     plt.plot(np.arange(num+1, len(percs)+1) , S.mean(0), 'o-', alpha = (1/len(percs))*(num+1), color='b')
@@ -11,4 +11,6 @@ for num in range(4):
 plt.grid()
 plt.xticks(np.arange(1,len(percs)+1), [f'{int(i*100)} %' for i in percs])
 plt.ylabel('RMSE')
+plt.savefig("./result_img/sample_augmentation.png")
 plt.show()
+
